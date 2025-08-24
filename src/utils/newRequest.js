bc50fa7c3326698125a25472c1a116a132f6c117
Nidhi932 <pandeynidhi9322@gmail.com>
2025-08-24 13:04:19 +0530

    commit

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL;  // From Vite env
+const API =
+  import.meta.env.VITE_API_URL ||
+  "https://driveclone-backend-production.up.railway.app"; // From Vite env
 
 const newRequest = axios.create({
-  baseURL: API,   // No need to add /api unless your backend is prefixed
+  baseURL: API, // No need to add /api unless your backend is prefixed
   withCredentials: true,
 });
 
